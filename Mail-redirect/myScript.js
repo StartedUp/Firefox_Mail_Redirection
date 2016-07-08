7191afee75ae709af338ff37aee236147ca8e851
Gokul Raj Murugasamy (GIS) <GO289350@wipro.com>
2016-07-08 19:28:53 +0530

    Web request permission enabled and myscript added

--- Mail-redirect/myScript.js
+++ Mail-redirect/myScript.js
@@ -1,1 +1,8 @@
-document.body.style.border = "5px solid red";
+function logURL(requestDetails) {
+  console.log("Loading: " + requestDetails.url);
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {urls: ["<all_urls>"]}
+);
